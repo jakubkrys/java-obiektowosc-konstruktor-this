f768e45772ebd1c8c2a56835e72e52266ca19a31
Jakub <jakubkrys@gmail.com>
2021-01-11 19:00:24 +0100

    #3 Program przechowujący listę zadań

---
+++ src/Task.java
@@ -0,0 +1,11 @@
+public class Task {
+    char[] title;
+    char[] description;
+    boolean done;
+
+    Task (char[] title, char[] description, boolean done){
+        this.title = title;
+        this.description = description;
+        this.done = done;
+    }
+}
